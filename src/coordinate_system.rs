c13026670807f096835844199ccddb911e30b5b6
Osebhulimen Egbor <os614875@dal.ca>
2024-12-08 19:01:43 -0400

    Added math operations on coordinates

--- src/coordinate_system.rs
+++ src/coordinate_system.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::ops::{Add, AddAssign};
+use std::ops::{Add, AddAssign, Sub};
 use std::str::FromStr;
 
 /// # Coordinate System
@@ -22,7 +22,8 @@ use std::str::FromStr;
 /// ```
 /// use self::aoc_utils_rust::coordinate_system::Coordinate;
 /// let coord = Coordinate::new(3, 4);
-/// assert_eq!(coord.manhattan_distance(), 7);
+/// let origin = Coordinate::new(0, 0);
+/// assert_eq!(coord.manhattan_distance(origin), 7);
 /// ```
 ///
 /// Transposing coordinates:
@@ -85,21 +86,60 @@ impl Coordinate {
     /// assert_eq!(coord.i, 3);
     /// assert_eq!(coord.j, 4);
     /// ```
+    #[inline(always)]
     pub const fn new(x: i32, y: i32) -> Self {
         Self { i: x, j: y }
     }
 
-    /// Calculates the Manhattan distance from the origin.
+    /// Calculates the Manhattan distance between two coordinates.
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - The other coordinate to calculate the distance to.
     ///
     /// # Examples
     ///
     /// ```
     /// use self::aoc_utils_rust::coordinate_system::Coordinate;
-    /// let coord = Coordinate::new(3, 4);
-    /// assert_eq!(coord.manhattan_distance(), 7);
+    /// let coord1 = Coordinate::new(3, 4);
+    /// let coord2 = Coordinate::new(1, 1);
+    /// assert_eq!(coord1.manhattan_distance(coord2), 5);
+    ///
+    /// let coord1 = Coordinate::new(3, 4);
+    /// let coord2 = Coordinate::new(0, 0);
+    /// assert_eq!(coord1.manhattan_distance(coord2), 7);
     /// ```
-    pub const fn manhattan_distance(&self) -> i32 {
-        self.i.abs() + self.j.abs()
+    #[inline]
+    pub const fn manhattan_distance(&self, other: Self) -> i32 {
+        let (dx, dy) = self.slope_relative(other);
+        dx.abs() + dy.abs()
+    }
+
+    /// Calculates the Euclidean distance between two coordinates.
+    ///
+    /// # Arguments
+    ///
+    /// * `other` - The other coordinate to calculate the distance to.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(3, 4);
+    /// let coord2 = Coordinate::new(0, 0);
+    /// assert_eq!(coord1.distance_to(coord2), 5.0);
+    /// ```
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(1, 1);
+    /// let coord2 = Coordinate::new(4, 5);
+    /// assert_eq!(coord1.distance_to(coord2), 5.0);
+    /// ```
+    #[inline]
+    pub fn distance_to(&self, other: Self) -> f64 {
+        let (dx, dy) = self.slope_relative(other);
+        ((dx * dx + dy * dy) as f64).sqrt()
     }
 
     /// Transposes the coordinate.
@@ -113,8 +153,102 @@ impl Coordinate {
     /// assert_eq!(transposed.i, 4);
     /// assert_eq!(transposed.j, 3);
     /// ```
+    #[inline(always)]
     pub const fn transpose(&self) -> Self {
         Self::new(self.j, self.i)
+    }
+
+    /// Calculates the relative slope between two coordinates.
+    ///
+    /// The slope is represented as a tuple of integers `(di, dj)` || `(rise, run)`, where `di` and `dj`
+    /// are the differences in the x and y coordinates, respectively.
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - The starting coordinate.
+    /// * `other` - The ending coordinate.
+    ///
+    /// # Returns
+    ///
+    /// A tuple `(di, dj)` representing the slope.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(4, 4);
+    /// let coord2 = Coordinate::new(1, 8);
+    /// assert_eq!(coord2.slope_relative(coord1), (3, -4));
+    ///
+    /// let coord1 = Coordinate::new(4, 4);
+    /// let coord2 = Coordinate::new(1, 8);
+    /// assert_eq!(coord1.slope_relative(coord2), (-3, 4));
+    /// ```
+    #[inline(always)]
+    pub const fn slope_relative(&self, other: Self) -> (i32, i32) {
+        let di = other.i - self.i; // Difference in x-coordinates
+        let dj = other.j - self.j; // Difference in y-coordinates
+        (di, dj) // Return the slope as a tuple
+    }
+
+    /// Calculates the slope between two coordinates.
+    ///
+    /// The slope is represented as a floating-point number. If the difference in x-coordinates (`dx`) is zero,
+    /// the function returns `f64::INFINITY` to represent an infinite slope.
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - The starting coordinate.
+    /// * `other` - The ending coordinate.
+    ///
+    /// # Returns
+    ///
+    /// A floating-point number representing the slope.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(3, 4);
+    /// let coord2 = Coordinate::new(6, 8);
+    /// assert_eq!(coord1.slope(coord2), Some(4.0 / 3.0));
+    /// ```
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(1, 1);
+    /// let coord2 = Coordinate::new(1, 5);
+    /// assert_eq!(coord1.slope(coord2), None);
+    /// ```
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(0, 0);
+    /// let coord2 = Coordinate::new(4, 2);
+    /// assert_eq!(coord1.slope(coord2), Some(0.5));
+    /// ```
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(2, 3);
+    /// let coord2 = Coordinate::new(5, 3);
+    /// assert_eq!(coord1.slope(coord2), Some(0.0));
+    /// ```
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(3, 4);
+    /// let coord2 = Coordinate::new(0, 0);
+    /// assert_eq!(coord1.slope(coord2), Some(4.0 / 3.0));
+    /// ```
+    #[inline(always)]
+    pub fn slope(&self, other: Self) -> Option<f64> {
+        let (dx, dy) = self.slope_relative(other);
+        if dx == 0 {
+            None
+        } else {
+            Some(dy as f64 / dx as f64)
+        }
     }
 }
 
@@ -159,6 +293,33 @@ impl Add for Coordinate {
     }
 }
 
+impl Sub for Coordinate {
+    type Output = Self;
+
+    /// Subtracts one `Coordinate` from another.
+    ///
+    /// # Arguments
+    ///
+    /// * `rhs` - The right-hand side `Coordinate` to subtract.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord1 = Coordinate::new(5, 7);
+    /// let coord2 = Coordinate::new(2, 3);
+    /// let result = coord1 - coord2;
+    /// assert_eq!(result.i, 3);
+    /// assert_eq!(result.j, 4);
+    /// ```
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            i: self.i - rhs.i,
+            j: self.j - rhs.j,
+        }
+    }
+}
+
 impl Add<direction::Direction> for Coordinate {
     type Output = Self;
 
@@ -194,8 +355,26 @@ impl From<(i32, i32)> for Coordinate {
     /// assert_eq!(coord.i, 3);
     /// assert_eq!(coord.j, 4);
     /// ```
+    #[inline(always)]
     fn from((i, j): (i32, i32)) -> Self {
         Self::new(i, j)
+    }
+}
+
+impl Into<(i32, i32)> for Coordinate {
+    /// Converts a `Coordinate` into a tuple.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use self::aoc_utils_rust::coordinate_system::Coordinate;
+    /// let coord = Coordinate::new(3, 4);
+    /// let tuple: (i32, i32) = coord.into();
+    /// assert_eq!(tuple, (3, 4));
+    /// ```
+    #[inline(always)]
+    fn into(self) -> (i32, i32) {
+        (self.i, self.j)
     }
 }
 

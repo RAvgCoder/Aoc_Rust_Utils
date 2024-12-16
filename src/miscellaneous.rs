81d7bcb2fcc28daead6344f6aceda4a066db2c03
Osebhulimen Egbor <os614875@dal.ca>
2024-12-16 19:00:38 -0400

    Modifications

--- src/miscellaneous.rs
+++ src/miscellaneous.rs
@@ -1,12 +1,13 @@
 use crate::grid::Grid;
 use std::fmt::{Debug, Display};
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::Write;
 
 // Give me a fn that dumps a grid to a file passing in the file name
 pub fn dump_grid_to_file<T, F, M, G>(
     grid: &G,
-    file_name: &str,
+    file_name: Option<&str>,
+    append: bool,
     own_func: Option<F>,
 ) -> std::io::Result<()>
 where
@@ -14,28 +15,37 @@ where
     F: Fn(&T) -> M,
     M: Display,
 {
-    dump_obj_to_file(file_name, |file| match &own_func {
-        Some(f) => {
-            for row in 0..grid.num_rows() {
-                for e in grid.get_row(row).unwrap() {
-                    write!(file, "{}", f(e))?;
+    dump_obj_to_file(
+        Some(file_name.unwrap_or("grid_output.txt")),
+        |file| match &own_func {
+            Some(f) => {
+                for row in 0..grid.num_rows() {
+                    for e in grid.get_row(row).unwrap() {
+                        write!(file, "{}", f(e))?;
+                    }
+                    writeln!(file)?;
                 }
-                writeln!(file)?;
-            }
 
-            Ok(())
-        }
-        None => {
-            writeln!(file, "{:?}", grid)
-        }
-    })
+                Ok(())
+            }
+            None => {
+                writeln!(file, "{:?}", grid)
+            }
+        },
+        append,
+    )
 }
 
-pub fn dump_obj_to_file<F>(file_name: &str, f: F) -> std::io::Result<()>
+pub fn dump_obj_to_file<F>(file_name: Option<&str>, f: F, append: bool) -> std::io::Result<()>
 where
     F: Fn(&mut File) -> std::io::Result<()>,
 {
-    let mut file = File::create(file_name)?;
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(append)
+        .create(true) // Creates the file if it doesn't exist
+        .open(file_name.unwrap_or("obj_dump.txt"))
+        .expect("Unable to open or create the file");
     f(&mut file)?;
     Ok(())
 }
